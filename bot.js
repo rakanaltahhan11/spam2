2245ad7f373090fe00719ff363b9f5dadc746ba4
Family111 <44900180+Family111@users.noreply.github.com>
2018-11-20 18:36:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,25 @@ const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
-const client5 = new Discord.Client();
-const client6 = new Discord.Client();
-const client7 = new Discord.Client();
-const client8 = new Discord.Client();
-const client9 = new Discord.Client();
-const client10 = new Discord.Client();
+
 
 client.on('ready', () => {
-   console.log(`----------------`);
-   console.log(` Bots Online ' `);
-   console.log(`----------------`);
+   console.log(` Bots Online  [1]' `);
 });
+
+client2.on('ready', () => {
+   console.log(` Bots Online  [2]' `);
+});
+
+client3.on('ready', () => {
+   console.log(` Bots Online  [3]' `);
+});
+
+client4.on('ready', () => {
+   console.log(` Bots Online  [4]' `);
+});
+
+
 
 
 client.on('message', message => {
@@ -45,47 +52,10 @@ client4.on('message', message => {
     }
 });
 
-client5.on('message', message => {
-    if(message.content === 'repme'){
-        message.channel.send('#rep <@460606140666085378>')
-    }
-});
-
-client6.on('message', message => {
-    if(message.content === 'repme'){
-        message.channel.send('#rep <@460606140666085378>')
-    }
-});
-
-client7.on('message', message => {
-    if(message.content === 'repme'){
-        message.channel.send('#rep <@460606140666085378>')
-    }
-});
-
-client8.on('message', message => {
-    if(message.content === 'repme'){
-        message.channel.send('#rep <@460606140666085378>')
-    }
-});
-
-client9.on('message', message => {
-    if(message.content === 'repme'){
-        message.channel.send('#rep <@460606140666085378>')
-    }
-});
-
-client10.on('message', message => {
-    if(message.content === 'repme'){
-        message.channel.send('#rep <@460606140666085378>')
-    }
-});
-
-
 
 client.on("message", message => {
   if (message.author.id != "460606140666085378") return;
-  if (message.content.split(' ')[0] == 'Wsay') {
+  if (message.content.split(' ')[0] == '1say') {
     message.delete();
     message.channel.sendMessage(message.content.substr(5));
   }
@@ -95,7 +65,7 @@ client.on("message", message => {
 
 client2.on("message", message => {
   if (message.author.id != "460606140666085378") return;
-  if (message.content.split(' ')[0] == 'Wsay') {
+  if (message.content.split(' ')[0] == '1say') {
     message.delete();
     message.channel.sendMessage(message.content.substr(5));
   }
@@ -105,7 +75,7 @@ client2.on("message", message => {
 
 client3.on("message", message => {
   if (message.author.id != "438862789302419464") return;
-  if (message.content.split(' ')[0] == 'Wsay') {
+  if (message.content.split(' ')[0] == '1say') {
     message.delete();
     message.channel.sendMessage(message.content.substr(5));
   }
@@ -113,72 +83,16 @@ client3.on("message", message => {
 
 client4.on("message", message => {
   if (message.author.id != "438862789302419464") return;
-  if (message.content.split(' ')[0] == 'Wsay') {
+  if (message.content.split(' ')[0] == '1say') {
     message.delete();
     message.channel.sendMessage(message.content.substr(5));
   }
 });
-
-
-client5.on("message", message => {
-  if (message.author.id != "460606140666085378") return;
-  if (message.content.split(' ')[0] == 'Wsay') {
-    message.delete();
-    message.channel.sendMessage(message.content.substr(5));
-  }
-});
-
-
-client6.on("message", message => {
-  if (message.author.id != "460606140666085378") return;
-  if (message.content.split(' ')[0] == 'Wsay') {
-    message.delete();
-    message.channel.sendMessage(message.content.substr(5));
-  }
-});
-
-
-client7.on("message", message => {
-  if (message.author.id != "460606140666085378") return;
-  if (message.content.split(' ')[0] == 'Wsay') {
-    message.delete();
-    message.channel.sendMessage(message.content.substr(5));
-  }
-});
-
-
-client8.on("message", message => {
-  if (message.author.id != "460606140666085378") return;
-  if (message.content.split(' ')[0] == 'Wsay') {
-    message.delete();
-    message.channel.sendMessage(message.content.substr(5));
-  }
-});
-
-
-client9.on("message", message => {
-  if (message.author.id != "460606140666085378") return;
-  if (message.content.split(' ')[0] == 'Wsay') {
-    message.delete();
-    message.channel.sendMessage(message.content.substr(5));
-  }
-});
-
-
-client10.on("message", message => {
-  if (message.author.id != "460606140666085378") return;
-  if (message.content.split(' ')[0] == 'Wsay') {
-    message.delete();
-    message.channel.sendMessage(message.content.substr(5));
-  }
-});
-
-
 
 
 
 client.on('message', message => { 
-if (message.content === 'Kspam') {
+if (message.content === '1spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
@@ -195,7 +109,7 @@ if (message.content === 'Kspam') {
 
 
 client3.on('message', message => { 
-if (message.content === 'Kspam') {
+if (message.content === '1spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
@@ -211,7 +125,7 @@ if (message.content === 'Kspam') {
 
 
 client2.on('message', message => {
-if (message.content === 'Kspam') {
+if (message.content === '1spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
@@ -226,7 +140,7 @@ if (message.content === 'Kspam') {
 
 
 client4.on('message', message => { 
-if (message.content === 'Kspam') {
+if (message.content === '1spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
@@ -238,103 +152,6 @@ if (message.content === 'Kspam') {
         }
       }
 });
-
-
-
-client5.on('message', message => { 
-if (message.content === 'Kspam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**wxy Spam **[ #" ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-
-
-client6.on('message', message => { 
-if (message.content === 'Kspam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**wxy Spam **[ #" ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-
-
-client7.on('message', message => { 
-if (message.content === 'Kspam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**wxy Spam **[ #" ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-
-
-client8.on('message', message => { 
-if (message.content === 'Kspam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**wxy Spam **[ #" ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-
-
-client9.on('message', message => { 
-if (message.content === 'Kspam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**wxy Spam **[ #" ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-
-
-client10.on('message', message => { 
-if (message.content === 'Kspam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**wxy Spam **[ #" ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
 
 
 
@@ -345,9 +162,3 @@ client.login(process.env.BOT_TOKEN);
 client2.login(process.env.BOT_TOKEN2);
 client3.login(process.env.BOT_TOKEN3);
 client4.login(process.env.BOT_TOKEN4);
-client5.login(process.env.BOT_TOKEN5);
-client6.login(process.env.BOT_TOKEN6);
-client7.login(process.env.BOT_TOKEN7);
-client8.login(process.env.BOT_TOKEN8);
-client9.login(process.env.BOT_TOKEN9);
-client10.login(process.env.BOT_TOKEN10);
